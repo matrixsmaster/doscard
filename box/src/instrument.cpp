97b0c093b94608516fe3dac9d0a0c316fda06244
Dmitry Soloviov <matrixsmaster@ya.ru>
2014-01-03 11:47:45 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/doscard/code/trunk@21 e05d8a66-e4c2-43e5-bab5-538d5072adc9

--- box/src/instrument.cpp
+++ box/src/instrument.cpp
@@ -1,7 +1,10 @@
 /*
  * instrument.cpp
  *
+ * Instrumentation module for DosCard Project analyze and profiling
+ *
  *  Created on: Jan 3, 2014
+ *  (C) Dmitry "MatrixS_Master" Soloviov, 2014
  */
 
 #include <stdio.h>
@@ -13,14 +16,14 @@
 #define PROFILE_UNIQUE_CALL_LIST 1
 //#define PROFILE_TREE_CALL_TRACE 1
 
-#define PROFILE_OUT_FILENAME "/tmp/boxprofile"
-#define PROFILE_LIST_GROW_BY 4096
+#define PROFILE_OUT_FILENAME "boxprofile"
+#define PROFILE_LIST_GROW_BY 8192
 
 static FILE* fd = NULL;
-static struct {
-	void* fun;
-	void* child;
-} * tree = NULL;
+//static struct {
+//	void* fun;
+//	void* child;
+//} * tree = NULL;
 static Bitu* list = NULL;
 static unsigned long long index = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -31,7 +34,11 @@ void __attribute__ ((constructor)) profile_create()
 	fd = fopen(PROFILE_OUT_FILENAME,"w");
 #elif (PROFILE_UNIQUE_CALL_LIST)
 	list = reinterpret_cast<Bitu*> (malloc(PROFILE_LIST_GROW_BY*sizeof(Bitu)));
-//	index = PROFILE_LIST_GROW_BY;
+	if (!list) {
+		fprintf(stderr,"[PROFILE]: FATAL: Unable to allocate list memory!\n");
+		abort();
+	}
+	index = 0;
 #elif (PROFILE_TREE_CALL_TRACE)
 #endif
 	printf("[PROFILE]: Started\n");
@@ -44,12 +51,13 @@ void __attribute__ ((destructor)) profile_destroy()
 #elif (PROFILE_UNIQUE_CALL_LIST)
 	fd = fopen(PROFILE_OUT_FILENAME,"w");
 	if (!fd) {
-		fprintf(stderr,"[PROFILE]: Unable to open profile output file!\n");
+		fprintf(stderr,"[PROFILE]: ERR: Unable to open profile output file!\n");
 		return;
 	}
 	void* ptr;
-	for (unsigned long long i=0; i<index; i++) {
+	for (Bit64u i=0; i<index; i++) {
 		ptr = reinterpret_cast<void*> (list[i]);
+//		printf("%p\n",ptr);
 		fprintf(fd,"%p\n",ptr);
 	}
 	free(list);
@@ -66,19 +74,22 @@ extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site)
 #elif (PROFILE_UNIQUE_CALL_LIST)
 	if (!list) return;
 	pthread_mutex_lock(&mutex);
-	for (unsigned long long i=0; i<index; i++)
-		if (list[i] == reinterpret_cast<Bitu> (this_fn)) {
-			pthread_mutex_unlock(&mutex);
-			return;
-		}
+	if (index > 1) {
+		for (Bit64u i=index-1; i>0; i--)
+//		for (Bit64u i=0; i<index; i++)
+			if (list[i] == reinterpret_cast<Bitu> (this_fn)) {
+				pthread_mutex_unlock(&mutex);
+				return;
+			}
+	}
 	if ((++index) % PROFILE_LIST_GROW_BY == 0) {
 		list = reinterpret_cast<Bitu*> (realloc(list,(index+PROFILE_LIST_GROW_BY)*sizeof(Bitu)));
 		if (list == NULL) {
-			fprintf(stderr,"\n[PROFILE]: OUT OF MEMORY!\n");
+			fprintf(stderr,"\n[PROFILE]: FATAL: OUT OF MEMORY!\n");
 			abort();
 		}
 	}
-	list[index] = reinterpret_cast<Bitu> (this_fn);
+	list[index-1] = reinterpret_cast<Bitu> (this_fn);
 	pthread_mutex_unlock(&mutex);
 #elif (PROFILE_TREE_CALL_TRACE)
 #endif
@@ -92,4 +103,3 @@ extern "C" void __cyg_profile_func_exit(void *this_fn, void *call_site)
 #elif (PROFILE_TREE_CALL_TRACE)
 #endif
 }
-
