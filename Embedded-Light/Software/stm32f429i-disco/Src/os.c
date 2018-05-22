f5db5ad470599be3c5dfcedbfe31a3b1a6450ad6
matrixsmaster <matrixsmaster@yandex.ru>
2018-05-22 20:44:16 +0300

    F4DISCO: Added new font data. Text is rendered correctly now.

--- Embedded-Light/Software/stm32f429i-disco/Src/os.c
+++ Embedded-Light/Software/stm32f429i-disco/Src/os.c
@@ -3,6 +3,26 @@
 #include <stdio.h>
 #include "main.h"
 #include "VM86Wrapper.h"
+#include "pfont.h"
+
+void OS_DrawChar(int col, int row, char x)
+{
+	if (x < 33 || x > 126) return;
+	__IO uint16_t* buf = LCD_GetCurrentBuffer();
+	int cx = (25-row)*8;
+	int cy = col*4;
+	for (int i = 0; i < 4; i++) {
+		uint8_t sym = font8x4_rotated[(x-33)*4+i];
+		for (int j = 0; j < 8; j++) {
+			if (sym & 0x80)
+				*(__IO uint16_t*)(buf+(LCD_PIXEL_WIDTH*cy+cx)) = 0; //FIXME: black color
+			sym <<= 1;
+			cx++;
+		}
+		cx = (25-row)*8;
+		cy++;
+	}
+}
 
 void OS()
 {
@@ -21,14 +41,17 @@ void OS()
 		if (r > 0x01000000) {
 			l = (r & 0x00FF0000) >> 16;
 			p = (r & 0x0000FF00) >> 8;
-			LCD_DrawFullRect((25-l)*8,p*4,8,4);
+			char bp = r & 0xFF;
+			//LCD_DrawFullRect((25-l)*8,p*4,8,4);
+			if (bp) OS_DrawChar(p,l,bp);
 
 		} else if (r == 1) {
 			LCD_Clear(LCD_COLOR_GREEN);
 			char* bp = VM86_GetShadowBuf();
 			for (l = 0; l < VM86W_GEOMH; l++)
 				for (p = 0; p < VM86W_GEOMW; p++,bp++) {
-					if (*bp && *bp != ' ') LCD_DrawFullRect((25-l)*8,p*4,8,4);
+					//if (*bp && *bp != ' ') LCD_DrawFullRect((25-l)*8,p*4,8,4);
+					if (*bp) OS_DrawChar(p,l,*bp);
 				}
 		}
 	}
